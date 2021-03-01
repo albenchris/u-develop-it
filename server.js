dec1c13964193543aa1650c445ff3f57841f2e8f
albenchris <albenchris00@gmail.com>
2021-03-01 14:11:43 -0600

    created function to save new candidate using inputCheck

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
+const inputCheck = require("./utils/inputCheck");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -53,12 +54,6 @@ app.get("/api/candidate/:id", (req, res) => {
 });
 
 // DELETE a candidate
-// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result, this, this.changes);
-// });
 app.delete("/api/candidate/:id", (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
     const params = [req.params.id];
@@ -76,15 +71,28 @@ app.delete("/api/candidate/:id", (req, res) => {
 });
 
 // CREATE a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-                VALUES (?,?,?,?)`;
-const params = [1, 'Roland', 'Firbank', 1];
-
-db.run(sql, params, function(err, result) {
-    if (err) {
-        console.log(err);
+app.post("/api/candidate", ({ body }, res) => {
+    const errors = inputCheck(body, "first_name", "last_name", "industry_connected");
+    if (errors) {
+        res.sendStatus(400).json({ error: errors });
+        return;
     }
-    console.log(result, this.lastID);
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.sendStatus(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: "success",
+            data: body,
+            id: this.lastID
+        });
+    });
 });
 
 // Default response for any other request (Not Found) catch all
