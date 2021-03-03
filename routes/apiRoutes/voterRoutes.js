1422755a7ac5f9c38affaf09ad0fe839b2791065
albenchris <albenchris00@gmail.com>
2021-03-02 18:39:04 -0600

    function to create a voter

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -5,6 +5,7 @@ const inputCheck = require("../../utils/inputCheck");
 
 // ========== VOTERS START ==========================================================
 
+// GET all voters
 router.get("/voters", (req, res) => {
     const sql = `SELECT * FROM voters ORDER BY last_name`;
     const params = [];
@@ -22,6 +23,7 @@ router.get("/voters", (req, res) => {
     });
 });
 
+// GET a specific voter
 router.get("/voter/:id", (req, res) => {
     const sql = `SELECT * FROM voters WHERE id = ?`;
     const params = [req.params.id];
@@ -38,6 +40,38 @@ router.get("/voter/:id", (req, res) => {
         });
     });
 });
+
+// CREATE a voter
+router.post("/voter", ({ body }, res) => {
+    const errors = inputCheck(body, "first_name", "last_name", "email");
+    if (errors) {
+        res.sendStatus(400).json({ error: errors });
+        return;
+    }
+    
+    const sql = `INSERT INTO voters (first_name, last_name, email)
+                    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.run(sql, params, function(err, data) {
+        if (err) {
+            res.sendStatus(400).json({ error: message });
+            return;
+        }
+
+        res.json({
+            message: "success",
+            data: body,
+            id: this.lastID
+        });
+    });
+});
+
+// UPDATE a voter
+
+
+// DELETE a voter
+
 
 // ========== VOTERS END ============================================================
 
